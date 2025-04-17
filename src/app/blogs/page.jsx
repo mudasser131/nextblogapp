501c82064de7c494a3883ece0cdcdc62f5c0fbb5
khaja shija uddin <83953366+mudasser131@users.noreply.github.com>
2025-04-17 21:40:38 +0530

    Update page.jsx

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -16,7 +16,7 @@ export default async function Page() {
   const data = await getData();
 
   return (
-    <div className="max-w-4xl mx-auto p-7">
+    <div className="max-w-4xl mx-auto p-6">
       <h1 className="text-3xl font-bold text-center mb-8">Latest Blog Posts</h1>
 
       <div className="grid md:grid-cols-2 gap-6">
