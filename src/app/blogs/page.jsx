766e701c69c7438d2442cfe84118210af1e1266c
mudasser <khajashija@gmail.com>
2025-04-20 21:48:06 +0530

    ui

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -17,7 +17,7 @@ export default async function Page() {
 
   return (
     <div className="max-w-4xl mx-auto p-6">
-      <h1 className="text-3xl font-bold text-center mb-8">Latest Blog Posts</h1>
+      <h1 className="text-3xl font-bold text-center mb-7">Latest Blog Posts</h1>
 
       <div className="grid md:grid-cols-2 gap-6">
         {data.map((item) => (
